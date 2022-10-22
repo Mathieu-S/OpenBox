9ac4b27ffc9c8d05cc7edf2c31460c6e042ea007
Mathieu-S <10650749+Mathieu-S@users.noreply.github.com>
2022-10-23 00:56:36 +0200

    Update doc

--- src/OpenBox.Application/Common/ICommandHandler.cs
+++ src/OpenBox.Application/Common/ICommandHandler.cs
@@ -1,10 +1,19 @@
 namespace OpenBox.Application.Common;
 
+/// <summary>
+/// Represent an handler of a command without a return value.
+/// </summary>
+/// <typeparam name="TCommand">The type of command.</typeparam>
 public interface ICommandHandler<in TCommand>
 {
     Task Handle(TCommand command, CancellationToken ct);
 }
 
+/// <summary>
+/// Represent an handler of a command with a return value.
+/// </summary>
+/// <typeparam name="TCommand">The type of command.</typeparam>
+/// <typeparam name="TResult">The return type value.</typeparam>
 public interface ICommandHandler<in TCommand, TResult>
 {
     Task<TResult> Handle(TCommand command, CancellationToken ct);
--- src/OpenBox.Application/Common/IQueryHandler.cs
+++ src/OpenBox.Application/Common/IQueryHandler.cs
@@ -1,5 +1,10 @@
 namespace OpenBox.Application.Common;
 
+/// <summary>
+/// Represent an handler of a query.
+/// </summary>
+/// <typeparam name="T">The type of query.</typeparam>
+/// <typeparam name="TResult">The return type value.</typeparam>
 public interface IQueryHandler<in T, TResult>
 {
     Task<TResult> Handle(T query, CancellationToken ct);
